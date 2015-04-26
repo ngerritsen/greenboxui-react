5e65ff79a5e6d915a8bc558f91125f8558b268ba
Aesthetickz <n.c.gerritsen@gmail.com>
2015-04-26 15:01:02 +0200

    Half baked solution for in code translation of strings (in React components)

--- app/shared/grid/grid-toolbar.jsx
+++ app/shared/grid/grid-toolbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Immutable from 'immutable';
 import Translator from '../../translation/translator';
+import TranslationStore from '../../translation/translation-store';
 
 export default React.createClass({
     propTypes: {
         onSearch: React.PropTypes.func.isRequired,
         columnInfo: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+    },
+    getInitialState() {
+        return { translatedStrings: Immutable.Map({
+            search: 'search',
+            all: 'all'
+        }) }
+    },
+    componentDidMount() {
+        TranslationStore.listen(this._onChange);
+        this._onChange();
+    },
+    componentWillUnmount() {
+        TranslationStore.unlisten(this._onChange)
+    },
+    _onChange() {
+        let translatedStrings = this.state.translatedStrings.set('search', TranslationStore.translate('search'));
+        translatedStrings = translatedStrings.set('all', TranslationStore.translate('all'));
+        this.setState({
+            translatedStrings: translatedStrings
+        });
     },
     _handleSearch() {
         const newSearchParameter = React.findDOMNode(this.refs.searchInput).value.trim();
@@ -15,6 +37,7 @@ export default React.createClass({
         const searchOptions = this.props.columnInfo.map((column) => {
             return <option value={column.id} key={column.id}>{column.title}</option>;
         });
+        const translatedStrings = this.state.translatedStrings;
         return (
             <form>
                 <div className="row">
@@ -25,14 +48,14 @@ export default React.createClass({
                             </div>
                             <div className="small-10 columns">
                                 <select ref="searchBySelection" onChange={this._handleSearch}>
-                                    <option value={''} key={'all'}>All</option>
+                                    <option value={''} key={'all'}>{translatedStrings.get('all')}</option>
                                 {searchOptions}
                                 </select>
                             </div>
                         </div>
                     </div>
                     <div className="small-8 columns">
-                        <input type="text" id="search" placeholder="Search" ref="searchInput" onChange={this._handleSearch}></input>
+                        <input type="text" id="search" placeholder={translatedStrings.get('search')} ref="searchInput" onChange={this._handleSearch}></input>
                     </div>
                 </div>
             </form>
